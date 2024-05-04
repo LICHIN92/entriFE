aa57982ca8888611b5d5e407821132f681bb8526
LICHIN.C <lichinchandran1432gmail.com>
2024-05-04 15:49:43 -0700

    /

--- src/entriNews/Articles.jsx
+++ src/entriNews/Articles.jsx
@@ -3,24 +3,25 @@ import Article from './Article'
 
 const Articles = () => {
   return (
-   <section className='articles'>
-      <Article 
-      id={'01'} title={'Reviving Retro PCs'}
-       desc={' What happens when old PCs are given modern upgrades?'} 
-       img={"src/assets/images/image-retro-pcs.jpg"}/>
+    <section className='articles'>
+      <Article
+        id={'01'} title={'Reviving Retro PCs'}
+        desc={' What happens when old PCs are given modern upgrades?'}
+        img={"/src/assets/images/image-retro-pcs.jpg"} />
 
-<Article 
-      id={'02'}
-       title='Top 10 Laptops of 2022'
-       desc='Our best picks for various needs and budgets.'
-        img={"src/assets/images/image-top-laptops.jpg"}/>
+      <Article
+        id={'02'}
+        title='Top 10 Laptops of 2022'
+        desc='Our best picks for various needs and budgets.'
+        img={"src/assets/images/image-top-laptops.jpg"} 
+        />
 
-<Article 
-      id={'03'}
-       title={'The Growth of Gaming'} 
-       desc={'How the pandemic has sparked fresh opportunities.'}
-        img={"src/assets/images/image-retro-pcs.jpg"}/>
-   </section>
+      <Article
+        id={'03'}
+        title={'The Growth of Gaming'}
+        desc={'How the pandemic has sparked fresh opportunities.'}
+        img={"src/assets/images/image-retro-pcs.jpg"} />
+    </section>
   )
 }
 
